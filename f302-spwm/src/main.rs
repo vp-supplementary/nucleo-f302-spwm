42f477da969737bf70df87858ca43ce9a53ee780
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2025-07-31 23:28:29 +0200

    main: Added second channel initialization

--- f302-spwm/src/main.rs
+++ f302-spwm/src/main.rs
@@ -8,7 +8,7 @@ use cortex_m::peripheral::NVIC;
 use cortex_m_rt::entry;
 use stm32f3::stm32f302::{Interrupt, gpioc::moder::MODE, interrupt};
 
-use spwm::Spwm;
+use spwm::{Spwm, SpwmState};
 use stm32f3::stm32f302::rcc::cfgr::SW;
 
 #[panic_handler]
@@ -28,7 +28,7 @@ impl<T> UnsafeSingleton<T> {
         }
     }
 
-    fn get_peripheral(&self) -> &T {
+    fn get_ref(&self) -> &T {
         unsafe { self.holder.get().as_ref().unwrap().as_ref().unwrap() }
     }
 }
@@ -46,7 +46,7 @@ static TIM15_SOFTWARE_PWM: UnsafeSingleton<Spwm> = UnsafeSingleton {
 };
 
 fn clock_init() {
-    let peripheral = PERIPHERAL_SINGLETON.get_peripheral();
+    let peripheral = PERIPHERAL_SINGLETON.get_ref();
 
     if peripheral.RCC.cr().read().hsirdy().is_not_ready() {
         peripheral.RCC.cr().modify(|_, w| w.hsion().set_bit());
@@ -58,19 +58,20 @@ fn clock_init() {
 
 fn gpio_init() {
     PERIPHERAL_SINGLETON
-        .get_peripheral()
+        .get_ref()
         .RCC
         .ahbenr()
         .modify(|_, w| w.iopcen().set_bit());
-    PERIPHERAL_SINGLETON
-        .get_peripheral()
-        .GPIOC
-        .moder()
-        .modify(|_, w| w.moder9().set(MODE::Output as u8));
+    PERIPHERAL_SINGLETON.get_ref().GPIOC.moder().modify(|_, w| {
+        w.moder9()
+            .set(MODE::Output as u8)
+            .moder8()
+            .set(MODE::Output as u8)
+    });
 }
 
 fn tim15_init() {
-    let peripheral = PERIPHERAL_SINGLETON.get_peripheral();
+    let peripheral = PERIPHERAL_SINGLETON.get_ref();
 
     peripheral
         .RCC
@@ -89,18 +90,73 @@ fn tim15_init() {
     unsafe { NVIC::unmask(Interrupt::TIM1_BRK_TIM15) };
 }
 
+fn pc9_period_callback() {}
+
+fn pc9_on_off_callback(state: &SpwmState) {
+    let peripheral = PERIPHERAL_SINGLETON.get_ref();
+    let gpioc = &peripheral.GPIOC;
+
+    match state {
+        SpwmState::On => {
+            gpioc.bsrr().write(|w| w.bs9().set_bit());
+        }
+        SpwmState::Off => {
+            gpioc.bsrr().write(|w| w.br9().set_bit());
+        }
+    }
+}
+
+fn pc8_period_callback() {}
+
+fn pc8_on_off_callback(state: &SpwmState) {
+    let peripheral = PERIPHERAL_SINGLETON.get_ref();
+    let gpioc = &peripheral.GPIOC;
+
+    match state {
+        SpwmState::On => {
+            gpioc.bsrr().write(|w| w.bs8().set_bit());
+        }
+        SpwmState::Off => {
+            gpioc.bsrr().write(|w| w.br8().set_bit());
+        }
+    }
+}
+
 #[entry]
 fn main() -> ! {
     unsafe {
         PERIPHERAL_SINGLETON.set(Some(stm32f3::stm32f302::Peripherals::steal()));
         CORE_PERIPHERAL_SINGLETON.set(Some(cortex_m::Peripherals::steal()));
+        TIM15_SOFTWARE_PWM.set(Some(Spwm::new(100_000, None, None)));
     }
+
+    // 100 kHz = 10 us
+    // 10 Hz = 100 ms = 100000 us
+    // 100 kHz / 10 Hz = 10000 tick -> 10000 ticks * 10 us = 100000 us = 100 ms
+    // channel #0
+    let _ = TIM15_SOFTWARE_PWM.get_ref().set_channel_frequency(0, 10);
+    let _ = TIM15_SOFTWARE_PWM
+        .get_ref()
+        .set_channel_on_off_callback(0, pc9_on_off_callback);
+    let _ = TIM15_SOFTWARE_PWM
+        .get_ref()
+        .set_channel_period_callback(0, pc9_period_callback);
+    let _ = TIM15_SOFTWARE_PWM.get_ref().set_channel_duty_cycle(0, 90);
+    let _ = TIM15_SOFTWARE_PWM.get_ref().enable(0);
+    // channel #1
+    let _ = TIM15_SOFTWARE_PWM.get_ref().set_channel_frequency(1, 240);
+    let _ = TIM15_SOFTWARE_PWM
+        .get_ref()
+        .set_channel_on_off_callback(1, pc8_on_off_callback);
+    let _ = TIM15_SOFTWARE_PWM
+        .get_ref()
+        .set_channel_period_callback(1, pc8_period_callback);
+    let _ = TIM15_SOFTWARE_PWM.get_ref().set_channel_duty_cycle(1, 50);
+    let _ = TIM15_SOFTWARE_PWM.get_ref().enable(1);
 
     clock_init();
     gpio_init();
     tim15_init();
-
-    // let mut toggle = false;
 
     loop {
         cortex_m::asm::wfi();
@@ -109,21 +165,9 @@ fn main() -> ! {
 
 #[interrupt]
 fn TIM1_BRK_TIM15() {
-    static mut TOGGLE: bool = false;
-    let peripheral = PERIPHERAL_SINGLETON.get_peripheral();
+    let peripheral = PERIPHERAL_SINGLETON.get_ref();
     let tim15 = &peripheral.TIM15;
-    let gpioc = &peripheral.GPIOC;
 
     tim15.sr().write(|w| w.uif().clear_bit());
-
-    let current_state = *TOGGLE;
-    *TOGGLE = current_state.not();
-
-    if current_state {
-        gpioc.bsrr().write(|w| w.bs9().set_bit());
-        // gpioc.bsrr().write(|w| w.br9().set_bit());
-    } else {
-        // gpioc.bsrr().write(|w| w.bs9().set_bit());
-        gpioc.bsrr().write(|w| w.br9().set_bit());
-    }
+    TIM15_SOFTWARE_PWM.get_ref().irq_handler();
 }
